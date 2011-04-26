7094cb1a5c18720f11c8a99929eb6c2086f28aa4
Hannu Parviainen <hpparvi@gmail.com>
2011-04-26 13:15:22 +0100

    Cleanup of the multitransitfitter with fixes to MPI handling.

--- fitting/multitransitfitter.py
+++ fitting/multitransitfitter.py
@@ -26,7 +26,7 @@ from matplotlib import pyplot as pl
 from transitLightCurve.core import *
 from transitLightCurve.transitparameterization import TransitParameterization, parameters, parameterizations
 from transitLightCurve.transitlightcurve import TransitLightcurve
-from transitLightCurve.fitting.de import DiffEvol, MPIDiffEvol
+from transitLightCurve.fitting.de import DiffEvol, ParallelDiffEvol
 from transitLightCurve.lightcurvedata import MultiTransitLC
 
 from fitparameterization import MTFitParameterization
@@ -34,23 +34,31 @@ from fitnessfunction import FitnessFunction
 
 class MTFitResult(FitResult):
     def __init__(self, fit_prm, res_ds, prm_ds, lcdata):
-        self.n_channels = {'value':fit_prm.nch, 'description':'Number of channels'}
-        self.ephemeris  = {'value':prm_ds.pv, 'description':'Ephemeris'}
-        self.limb_darkening =  {'value':[[fit_prm.get_ldc(i, res_ds)] for i in range(fit_prm.nch)],
-                                'description':'Limb darkening coefficients per channel.'}
+        self.n_channels = fit_prm.nch
+        self.channel_names = None
+        self.ephemeris  = prm_ds.pv
+        self.limb_darkening = [[fit_prm.get_ldc(i, res_ds)] for i in range(fit_prm.nch)]
+        self.zeropoint = [[fit_prm.get_zp(i, res_ds)] for i in range(fit_prm.nch)]
 
-        self.zeropoint = {'value':[[fit_prm.get_zp(i, res_ds)] for i in range(fit_prm.nch)],
-                                'description':'Zeropoint.'}
+        self.chi_sqr = None
+        self.de_population = None
+        self.de_fitness    = None
 
-        self.nch = self.n_channels['value']
-        self.e   = self.ephemeris['value']
-        self.ldc = self.limb_darkening['value']
-        self.zp  = self.zeropoint['value']
+        self.fitness_function = None
+        
+        self.nch = self.n_channels
+        self.e   = self.ephemeris
+        self.ldc = self.limb_darkening
+        self.zp  = self.zeropoint
 
-    def get_fit(self):
-        return self.res_ds
+
+    def get_fit(self, ch=0):
+        return self.ephemeris, self.limb_darkening[ch], self.zeropoint[ch]
 
     def get_chi(self):
+        return self.chi_sqr
+
+    def get_lc(self):
         pass
 
     def save(self, filename):
@@ -78,7 +86,7 @@ def fit_multitransit(lcdata, bounds, stellar_prm, **kwargs):
     third law, and then the Kipping's transit width parameter using the period and squared impact
     parameter.
     """
-    info('Starting multitransit fitting',H1)
+    if is_root: info('Starting multitransit fitting',H1)
 
     #######################################################################################
     ##
@@ -102,16 +110,17 @@ def fit_multitransit(lcdata, bounds, stellar_prm, **kwargs):
     p = MTFitParameterization(bounds, stellar_prm, nchannels, lcdata[0].n_transits, **kwargs)
     ##FIXME: Having different number of transits for each channel breaks things up.
 
-    info('Fitting data with',I1)
-    info('%6i free parameters'%p.p_cur.size, I2)
-    info('%6i channels'%nchannels, I2)
-    info('%6i datapoints'%totpoints, I2)
-    info('%6i levels of freedom'%(totpoints-p.p_cur.size), I2)
-    info('')
+    if is_root:
+        info('Fitting data with',I1)
+        info('%6i free parameters'%p.p_cur.size, I2)
+        info('%6i channels'%nchannels, I2)
+        info('%6i datapoints'%totpoints, I2)
+        info('%6i levels of freedom'%(totpoints-p.p_cur.size), I2)
+        info('')
 
     ## Setup the minimization function
     ## -------------------------------
-    minfun = FitnessFunction(p, lcdata)
+    minfun = FitnessFunction(p, lcdata, **kwargs)
 
     #######################################################################################
     ##
@@ -121,6 +130,8 @@ def fit_multitransit(lcdata, bounds, stellar_prm, **kwargs):
     ## Global fitting using differential evolution
     ## -------------------------------------------
     fitter_g = DiffEvol(minfun, array([p.p_min,p.p_max]).transpose(), **de_pars)
+    #fitter_g = ParallelDiffEvol(minfun, array([p.p_min,p.p_max]).transpose(), **de_pars)
+
     r_de     = fitter_g()
 
     f_de     = r_de.get_fit()
@@ -148,106 +159,52 @@ def fit_multitransit(lcdata, bounds, stellar_prm, **kwargs):
     ##
     ## Print parameters and statistics
     ## -------------------------------
-    info('Best-fit physical parameters',H2)
-    info("%14s %14s"%("DiffEvol" ,"FMin"),I1)
-    nc = nchannels if p.separate_k2_ch else 1 
-    nt = lcdata[0].n_transits if p.separate_zp_tr else 1
-    for chn in range(nc):
-        info("%14.5f %14.5f  -  radius ratio"%(p.get_physical(chn, f_de)[0],
-                                               p.get_physical(chn, f_fn)[0]), I1)
+    if is_root:
+        info('Best-fit physical parameters',H2)
+        info("%14s %14s"%("DiffEvol" ,"FMin"),I1)
+        nc = nchannels if p.separate_k2_ch else 1 
+        nt = lcdata[0].n_transits if p.separate_zp_tr else 1
+        for chn in range(nc):
+            info("%14.5f %14.5f  -  radius ratio"%(p.get_physical(chn, f_de)[0],
+                                                   p.get_physical(chn, f_fn)[0]), I1)
 
-    for i,k in enumerate(parameterizations['physical'][1:]):
-        info("%14.5f %14.5f  -  %s" %(p_de[i+1], p_fn[i+1], parameters[k].description), I1)
+        for i,k in enumerate(parameterizations['physical'][1:]):
+            info("%14.5f %14.5f  -  %s" %(p_de[i+1], p_fn[i+1], parameters[k].description), I1)
 
-    info("")
-    for chn in range(nchannels):
-        for tr in range(nt):
-            info("%14.5f %14.5f  -  zeropoint ch %i"%(p.get_zp(chn, tr, f_de),
-                                                      p.get_zp(chn, tr, f_fn), chn), I1)
+        info("")
+        for chn in range(nchannels):
+            for tr in range(nt):
+                info("%14.5f %14.5f  -  zeropoint ch %i"%(p.get_zp(chn, tr, f_de),
+                                                          p.get_zp(chn, tr, f_fn), chn), I1)
 
-    info("")
-    nc = nchannels if p.separate_ld else 1 
-    for chn in range(nc):
-        ldc_de  = p.get_ldc(chn, f_de)
-        ldc_fn  = p.get_ldc(chn, f_fn)
-        if p.n_ldc == 1:
-            info("%14.5f %14.5f  -  limb darkening ch %i u"%(ldc_de[0], ldc_fn, chn), I1)
-        if p.n_ldc == 2:
-            info(" % 5.3f % 5.3f  % 5.3f % 5.3f  -  limb darkening ch %i u v"%(ldc_de[0], ldc_de[1], ldc_fn[0], ldc_fn[1], chn), I1)
+        info("")
+        nc = nchannels if p.separate_ld else 1 
+        for chn in range(nc):
+            ldc_de  = p.get_ldc(chn, f_de)
+            ldc_fn  = p.get_ldc(chn, f_fn)
+            if p.n_ldc == 1:
+                info("%14.5f %14.5f  -  limb darkening ch %i u"%(ldc_de, ldc_fn, chn), I1)
+            if p.n_ldc == 2:
+                info(" % 5.3f % 5.3f  % 5.3f % 5.3f  -  limb darkening ch %i u v"%(ldc_de[0], ldc_de[1], ldc_fn[0], ldc_fn[1], chn), I1)
 
-    if p.fit_ttv:
-        p_ttv_de = p.get_ttv(f_fn)
-        p_ttv_fn = p.get_ttv(f_fn)
+        if p.fit_ttv:
+            p_ttv_de = p.get_ttv(f_fn)
+            p_ttv_fn = p.get_ttv(f_fn)
 
-        info("%14.5f %14.5f  -  TTV amplitude [min]"%(p_ttv_de[0]*1440, p_ttv_fn[0]*1440), I1)
-        info("%14.5f %14.5f  -  TTV period [d]"%(p_ttv_de[1]/TWO_PI, p_ttv_fn[1]/TWO_PI), I1)
-
-
-    info('Best-fit fitting parameters',H2)
-    for idx, pn in enumerate(p.fitted_parameter_names):
-        info('%10s %14.5f'%(pn, p.fitted_parameters[idx]))
-
-    info('Fit statistics',H2)
-    info('Differential evolution minimum %10.2f'%chi_de,I1)
-    info('Downhill simplex minimum       %10.2f'%chi_fn,I1)
-    info('')
-    info("Akaike's information criterion %10.2f" %(chi_fn + 2*p.p_cur.size), I1)
-    info('')
-
-    return MTFitResult(p, f_fn, p_fn, lcdata)
+            info("%14.5f %14.5f  -  TTV amplitude [min]"%(p_ttv_de[0]*1440, p_ttv_fn[0]*1440), I1)
+            info("%14.5f %14.5f  -  TTV period [d]"%(p_ttv_de[1]/TWO_PI, p_ttv_fn[1]/TWO_PI), I1)
 
 
+        info('Best-fit fitting parameters',H2)
+        for idx, pn in enumerate(p.fitted_parameter_names):
+            info('%10s %14.5f'%(pn, p.fitted_parameters[idx]))
 
+        info('Fit statistics',H2)
+        info('Differential evolution minimum %10.2f'%chi_de,I1)
+        info('Downhill simplex minimum       %10.2f'%chi_fn,I1)
+        info('')
+        info("Akaike's information criterion %10.2f" %(chi_fn + 2*p.p_cur.size), I1)
+        info('')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#    import pylab as pl
-#    for chn, (time,flux,pntn,sls) in enumerate(zip(times,fluxes,pntns,slices)):
-#        pl.subplot(3,1,chn+1)
-#        for trn, sl in enumerate(sls):
-#            pl.plot(pntn[sl], flux[sl]/p.get_zp(chn, trn) - 1, '.', c='0')
-#            pl.plot(pntn[sl], lc(time[sl], p.get_kipping(chn, f_fn), p.get_ldc(chn, f_fn)), lw=5, c='0')
-#            pl.plot(pntn[sl], lc(time[sl], p.get_kipping(chn, f_fn), p.get_ldc(chn, f_fn)), lw=2, c='.9')
-#        pl.ylim(-0.04, 0.01)
-#             t = linspace(time[sl][0], time[sl][-1],200)
-#             dt = t[-1]-t[0]
-#             x  = (t-t[0])/dt
-#             pl.plot((1.-x)*pntn[sl][0] + x*pntn[sl][-1], lc(t, p.get_kipping(chn, f_fn), p.get_ldc(chn, f_fn)), c='r')
-    
-#    pl.show()
-    #exit()
-
-#    lc = TransitLightcurve(TransitParameterization("kipping", p.p_k_min),
-#                           method=method, mode='phase', ldpar=bounds['ld'][0])
-#    ph = TWO_PI*linspace(-0.03,0.03,500)
-#    for chn in range(nchannels):
-#        pl.plot(ph, lc(ph, p.get_kipping(chn, r_fn[0]), p.get_ldc(chn, r_fn[0])))
-#    pl.show()
-#    exit()
-
-
+    if is_root:
+        return MTFitResult(p, f_fn, p_fn, lcdata)
