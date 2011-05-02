0342539281a61b28dbc268f548cdd0a1dbf407a5
Hannu Parviainen <hannu.p.parviainen@helsinki.fi>
2011-05-02 18:50:55 +0100

    Started implementing a curses based ui for mcmc.

--- fitting/mcmc.py
+++ fitting/mcmc.py
@@ -80,7 +80,10 @@ class MCMC(object):
             self.n_chains_l = self.n_chains//mpi_size
             if mpi_rank == mpi_size-1: self.n_chains_l += self.n_chains % mpi_size
             logging.info('Created node %i with %i chains'%(mpi_rank, self.n_chains_l))
-
+        else:
+            self.n_chains_g = self.n_chains
+            self.n_chains_l = self.n_chains
+            
         np.random.seed(self.seed)
 
         self.chifun = chifun
@@ -143,22 +146,28 @@ class MCMC(object):
 
 
     def __call__(self):
+        curses.wrapper(self._mcmc)
+
+    def _mcmc(self, *args):
         """The main Markov Chain Monte Carlo routine in all its simplicity."""
-        
+        self.init_curses()
+
         ## MCMC main loop
         ## ==============
         for chain in xrange(self.n_chains_l):
-            logging.info('Starting node %2i  chain %2i  of %2i' %(mpi_rank, chain+1, self.n_chains_l))
+            self.i_chain = chain
+            #logging.info('Starting node %2i  chain %2i  of %2i' %(mpi_rank, chain+1, self.n_chains_l))
             P_cur = self.p.copy()
             prior_cur = asarray([p.prior(P_cur[i]) for i, p in enumerate(self.parameters)])
             X_cur = self.chifun(P_cur[:-1])
 
             at_test = np.zeros(self.n_parms, dtype=np.int)
-            
+ 
             i_at = 0
             P_try = P_cur.copy()
             prior_try = prior_cur.copy()
             for i_s in xrange(self.n_steps):
+                self.i_step = i_s
                 for i_t in xrange(self.thinning):
                     for i_p, p in enumerate(self.parameters):
                         P_try[i_p] = p.draw(P_cur[i_p])
@@ -181,13 +190,13 @@ class MCMC(object):
 
                     ## Autotuning
                     ## ==========
-                    if i_s*self.thinning + i_t < self.autotune_length and i_at == 100:
+                    if i_s*self.thinning + i_t < self.autotune_length and i_at == 25:
                         for i_p, p in enumerate(self.parameters):
-                            accept_ratio  = at_test[i_p]/100. 
+                            accept_ratio  = at_test[i_p]/25. 
                             accept_adjust = (1. - self.autotune_strength) + self.autotune_strength*4.* accept_ratio
                             p._draw_method.sigma *= accept_adjust
-                            info("Autotune: %i %6.4f %6.2f %6.2f"%(i_p, p._draw_method.sigma, accept_ratio, accept_adjust), I1)
-                        info('', I1)
+                            #info("Autotune: %i %6.4f %6.2f %6.2f"%(i_p, p._draw_method.sigma, accept_ratio, accept_adjust), I1)
+                        #info('', I1)
                         at_test[:] = 0.
                         i_at = 0
                     i_at  += 1
@@ -197,8 +206,8 @@ class MCMC(object):
 
                 ## DEBUGGING CODE
                 ## ==============
-                if i_s!=0 and i_s%250 == 0:
-                    print "%2i %5i"%(mpi_rank, i_s), self.result.get_acceptance()
+                if i_s!=0 and i_s%25 == 0:
+                    #print "%2i %5i"%(mpi_rank, i_s), self.result.get_acceptance()
                     pl.figure(10, figsize=(20,20))
                     pl.clf()
                     for ip in range(self.n_parms):
@@ -208,6 +217,8 @@ class MCMC(object):
                         pl.subplot(self.n_parms,2,ip*2+2)
                         pl.hist(self.result.steps[0,:i_s,ip])
                         pl.savefig('mcmcdebug_n%i_%i.pdf'%(mpi_rank,chain))
+
+                self.ui.update()
 
         ## MPI communication
         ## =================
@@ -229,6 +240,41 @@ class MCMC(object):
 
     def get_results(self, parameter=None, burn_in=None, cor_len=None, separate_chains=False):
         return self.result(parameter, burn_in, cor_len, separate_chains)
+
+    def init_curses(self):
+        self.ui = MCMCCursesUI(self)
+
+class MCMCCursesUI(object):
+    def __init__(self, mcmc):
+        self.mcmc = mcmc
+        
+        self.screen = curses.initscr()
+        self.height, self.width = self.screen.getmaxyx()
+        self.main_window = CTitleWindow('MCMC', self.width, self.height, 0, 0)
+
+        ## Info window
+        self.info_window = CTitleWindow('Information', 40, 10, 3, 2)
+        self.iw = self.info_window
+        self.iw.addstr(1,1,'Chains')
+        self.iw.addstr(2,1,'Steps')
+        self.iw.addstr(4,1,'Autotune length {}'.format(self.mcmc.autotune_length))
+
+        self.accept_window = CTitleWindow('Accept ratio', self.width-6, 4, 3, 12)
+        self.aw = self.accept_window
+        self.aw.addstr(1,1,' '.join(['{0:^10s}'.format(n) for n in self.mcmc.p_names]))
+
+        self.sigma_window = CTitleWindow('Parameter Sigma', self.width-6, 4, 3, 16)
+        self.sw = self.sigma_window
+        self.sw.addstr(1,1,' '.join(['{0:^10s}'.format(n) for n in self.mcmc.p_names]))
+
+        self.fitted_p_window = CTitleWindow('Fitted parameters', self.width-6, 4, 3, 20)
+
+    def update(self):
+        self.iw.addstr(1,15,"{0:6d}/{1:6d}".format(self.mcmc.i_chain+1, self.mcmc.n_chains))
+        self.iw.addstr(2,15,"{0:6d}/{1:6d}".format(self.mcmc.i_step+1, self.mcmc.n_steps))
+
+        self.aw.addstr(2,1,' '.join(['{0:^10.1%}'.format(a) for a in self.mcmc.result.get_acceptance()]))
+        self.sw.addstr(2,1,' '.join(['{0:^10.6f}'.format(s) for s in [p._draw_method.sigma for p in self.mcmc.parameters]]))
 
 
 class MCMCResult(FitResult):
@@ -272,9 +318,7 @@ class MCMCResult(FitResult):
     def get_acceptance(self):
         """Returns the acceptance ratio for the parameters."""
         t = self.accepted.sum(0)
-        r = t[:, 1] / t[:, 0]
-        r[r!=r] = 0.
-        
+        r = np.where(t[:,0]>0, t[:,1]/t[:,0], 0)
         return r
 
     
