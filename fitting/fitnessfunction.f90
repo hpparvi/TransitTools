d06c0b2eee9802b55bef917e92ddf1c929535de3
Hannu Parviainen <hannu.p.parviainen@helsinki.fi>
2011-05-09 10:22:00 +0100

    Added the apply_zeropoint routine to the fortran fitness routines.

--- fitting/fitnessfunction.f90
+++ fitting/fitnessfunction.f90
@@ -20,8 +20,7 @@ contains
     !$omp parallel do private(i) shared(amplitude, ttv_period, period, time, transit_number, time_out, n) &
     !$omp schedule(static)
     do i=1,n
-       time_out(i) = amplitude*sin(6.283185307179586_fd * ttv_period * period * transit_number(i))
-       time_out(i) = time(i) + time_out(i)
+       time_out(i) = time(i) + amplitude*sin(6.283185307179586_fd * ttv_period * period * transit_number(i))
     end do
     !$omp end parallel do
   end subroutine calculate_time_with_ttv
@@ -40,5 +39,23 @@ contains
        chi_sqr = chi_sqr + (observed(i) - model(i))**2 * var_inv(i)
     end do
   end function chi_sqr
+
+  subroutine apply_zeropoints(zeropoints, chn_l, model, nchn, npt, res)
+    implicit none
+    integer, intent(in) :: nchn, npt
+    real(8), intent(in), dimension(npt)  :: model
+    real(8), intent(in), dimension(nchn) :: zeropoints
+    integer, intent(in), dimension(nchn) :: chn_l
+    real(8), intent(out), dimension(npt) :: res
+    integer j, i, i_s, i_e
+
+    i_s = 0
+    res(1:chn_l(1)) = zeropoints(1) * model(1:chn_l(1))
+    do j=2,nchn
+       i_s = i_s + chn_l(j-1)
+       i_e = i_s + chn_l(j)
+      res(i_s:i_e) = zeropoints(j) * model(i_s:i_e)
+    end do
+  end subroutine apply_zeropoints
 
 end module fitnessfunction
